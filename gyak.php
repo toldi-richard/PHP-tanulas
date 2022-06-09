f386d6eba2b150b43a6eb72aa7a91a39a3f724ae
toldi-richard <sze0tolric@vasvari.org>
2022-06-09 08:36:14 +0200

    filekezelés gyak

--- gyak.php
+++ gyak.php
@@ -1,9 +1,19 @@
 <?php
 
-$file = 'extras/valami.txt';
+$file = 'extras/valami2.txt';
 
 if (file_exists($file)) {
     $handle = fopen($file,'r');
     $content = fread($handle, filesize($file));
     echo $content;
+} else {
+    $handle = fopen($file,'w');
+    $content = 'nagyon' . PHP_EOL . 'jó' . PHP_EOL . 'minden' . PHP_EOL . 'koszi koszi';
+    fwrite($handle, $content);
+    fclose($handle);
 }
+
+    echo '<br>';
+    $handle = fopen($file,'r');
+    $content = fread($handle, filesize($file));
+    echo $content;
