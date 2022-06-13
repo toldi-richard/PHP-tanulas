fa736499f98c8a2b19f690fbc5af12bb483028bf
toldi-richard <sze0tolric@vasvari.org>
2022-06-13 10:37:18 +0200

    Feedback megjelenítés hard kódolva, db nélkül még

--- feedback/feedback.php
+++ feedback/feedback.php
@@ -1,23 +1,55 @@
 <?php include 'inc/header.php' ; ?>
+
+
+    <!-- átmeneti hard kódolt adatok, ha üres akkor az if felirat megjelenik -->
+    <?php 
+    $feedback = [
+      [
+        'id' => '1',
+        'name' => 'Beth Williams',
+        'email' => 'beth@gmail.com',
+        'body' => 'Traversy madia is ok'
+      ],
+      [
+        'id' => '2',
+        'name' => 'Jhon Smith',
+        'email' => 'jsmith@gmail.com',
+        'body' => 'Traversy madia is suck'
+      ],
+      [
+        'id' => '3',
+        'name' => 'Will Galvenzy',
+        'email' => 'wgalvenzy@gmail.com',
+        'body' => 'Traversy madia is working well'
+      ]
+    ]
+    ?>
    
-    <h2>Feedback</h2>
+    <h2>Past Feedback</h2>
 
-    <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
+
+    <!-- if ág így is használható, ha üres a tömb akkor megjelenik a felirat, mint a v-if, v-show vuejs-ben -->
+    <?php if (empty($feedback)):?>
+
+      <p class="lead mt3">There is no feedback</p>
+
+    <?php endif; ?>
+
+    <?php foreach ($feedback as $item): ?>
+
+    <div class="card my-3 w-75">
+     <div class="card-body text-center">
+
+      <?php echo $item['body'] ?>
+      
+      <div class="text-secondary mt-2">
+
+        By <?php echo $item['name'] ?>
+
+      </div>
      </div>
    </div>
 
-   <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-     </div>
-   </div>
-
-   <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-     </div>
-   </div>
+   <?php endforeach; ?>
    
 <?php include 'inc/footer.php' ; ?>
